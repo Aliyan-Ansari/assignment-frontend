5f06e4dc04fc179f2a877095b72830041eb9464d
UmerGillani36 <syedomershahgillani.com>
2023-03-02 06:57:16 -0800

    UI fixes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -43,9 +43,9 @@ const Home = () => {
         <div className={classes.MainCreateContainer}>
           <div className={classes.CreateContainer}>
           <div className={classes.CreateContentSection}>
-              <h4 style={{color:'blue', fontSize:'18px', margin:0}}>Construction</h4>
-              <h4 style={{color:'black', fontSize:'48px', margin:0}}>Concrete Trolley</h4>
-              <h4 style={{color:'black', fontSize:'10px', margin:0 , fontFamily:'Sarabun, sans-serif'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
+              <h4 style={{color:'blue', fontSize:'18px', margin:'0px 0px 10px 4px'}}>Construction</h4>
+              <h4 style={{color:'black', fontSize:'48px',margin:'0px 0px 10px 0px'}}>Concrete Trolley</h4>
+              <h4 style={{color:'black', fontSize:'12px',margin:'0px 0px 30px 0px' , fontFamily:'Sarabun, sans-serif'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
               <button  onClick={handleCreate} className={classes.action}>Create Order</button>
           </div>
           
